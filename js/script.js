7cf5847a4760ae3173f06a91928820f6c5d6fb07
dgichaba <dorothygichaba@gmail.com>
2023-02-06 06:54:08 +0300

    clean up files by removing unnecesarry lines of code

--- js/script.js
+++ js/script.js
@@ -4,11 +4,9 @@ function sendMessage(){
   var message=$("#message").val();
   if (name === "" || email==="" || message===""){
     $("#response").html("");
-    //$("#error").html("Please fill in all fields!");
  }else{
   $("#error").html("");
   $("#response").focus();
   $("#response").html("Thank you " +name+"! Your message has been received. We shall get back to you shorty");
-  //alert("Thank you " + name + "! Your message has been received. We shall get back to you shortly.");          
-};
+}
 }
